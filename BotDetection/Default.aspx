c98df3652a6974caf0ada476f44263bc0e2f7f0d
DanRGU <1308353@rgu.ac.uk>
2018-02-20 11:33:21 +0000

    Now retrieving JSON data from a given username - info output in textbox on default

--- BotDetection/Default.aspx
+++ BotDetection/Default.aspx
@@ -1,9 +1,11 @@
 ﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="BotDetection._Default" %>
 
 <asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+        <div id="inputContainer">
+            <asp:TextBox runat="server" ID="userInput" />
+            <asp:LinkButton ID="userSubmit" runat="server" OnClick="submitButton_Clicked"><span class="glyphicon glyphicon-search" aria-hidden="true"></span></asp:LinkButton>
+        </div>
 
-    <div id="inputContainer">
-            <input type="text" id="userInput" placeholder="@username"/>
-            <button type ="submit" id="userSubmit"><span class="glyphicon glyphicon-search" aria-hidden="true"></span></button>
-    </div>
+        <asp:TextBox runat="server" ID="twitterOutput" Text="" />
+
 </asp:Content>
--- BotDetection/Default.aspx.cs
+++ BotDetection/Default.aspx.cs
@@ -4,14 +4,66 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Net;
+using System.IO;
+using System.Text;
+using Tweetinvi;
+using Newtonsoft.Json;
 
 namespace BotDetection
 {
     public partial class _Default : Page
     {
+        string oAuthConsumerKey = "yCCy7XFwIGDxOppd7JsDMkmjt";
+        string oAuthConsumerSecret = "I8tLmmWblYLzRuHUSTtxaXmfHVLiKm0wGA0G8elR0SK34qor3D";
+        string oAuthAccessToken = "469737894-eMYLQcqUEyglEaURFMGgfW2IKYEVVSXZxcAwGZEh";
+        string oAuthAccessSecret = "C51EiVsmfGT08Auxl5wwYUkByDTsdy9sQxvXj2Mw5VJ1p";
+        
+
         protected void Page_Load(object sender, EventArgs e)
         {
 
         }
+
+        //Using TweetInvi to gather the Twitter timeline of the supplied account
+
+        protected void submitButton_Clicked(object sender, EventArgs e)
+        {
+            // Set up your credentials (https://apps.twitter.com)
+            Auth.SetUserCredentials(oAuthConsumerKey, oAuthConsumerSecret, oAuthAccessToken, oAuthAccessSecret);
+
+            var tweets = Timeline.GetUserTimeline(userInput.Text,1);
+            var json = Tweetinvi.JsonSerializer.ToJson(tweets);
+
+
+            if (tweets != null)
+            {
+                foreach (var tweet in tweets)
+                {
+                    twitterOutput.Text = json.ToString();
+                    break;
+                }
+            }
+            else {
+                twitterOutput.Text = "Private Account";
+            }
+        }
+    }
+
+    public class TwitterUser
+    {
+
+        public string ScreenName { get; set; }
+        public Tweets tweets{ get; set; }
+        public string postTime { get; set; }
+        public Array hashtags { get; set; }
+
+    }
+
+    public class Tweets {
+
+        public string content { get; set; }
+        
+
     }
 }
